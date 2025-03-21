01af51905c9a53e32268daf5d93d6ed3e36ca724
Ohad70 <ohad.t07@gmail.com>
2025-03-21 11:44:08 +0200

    save

--- app/src/main/java/com/example/firebase/HomeFragment.java
+++ app/src/main/java/com/example/firebase/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.firebase;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,6 +13,8 @@ import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
 
+    private WifiReceiver wifiReceiver;
+    
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,4 +22,27 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Create and register the BroadcastReceiver to listen for connectivity changes
+        wifiReceiver = new WifiReceiver();
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        getActivity().registerReceiver(wifiReceiver, filter);  // Use getActivity() to access the context
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Unregister the receiver when the fragment is paused
+        if (wifiReceiver != null) {
+            getActivity().unregisterReceiver(wifiReceiver);
+        }
+    }
 }
+
+
+
